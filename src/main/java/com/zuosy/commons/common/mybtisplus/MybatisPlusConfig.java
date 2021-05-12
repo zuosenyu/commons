47808e9f5177a99f9bcdf5b4d74da1617f35d60e
zuosy <15660616996@163.com>
2021-05-12 13:57:06 +0800

    huan jing da jian

---
+++ src/main/java/com/zuosy/commons/common/mybtisplus/MybatisPlusConfig.java
@@ -0,0 +1,24 @@
+package com.zuosy.commons.common.mybtisplus;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author: zuosy
+ * @date 2021/5/12 13:55
+ */
+@Configuration
+@MapperScan("com.zuosy.commons.mapper")
+public class MybatisPlusConfig {
+
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor(){
+        MybatisPlusInterceptor mybatisPlusInterceptor = new MybatisPlusInterceptor();
+        mybatisPlusInterceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        return mybatisPlusInterceptor;
+    }
+}
